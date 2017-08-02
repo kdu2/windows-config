494ab3e4bed814afe37a8adcc97f40eb9915c5a8
kdu2 <kdu2@users.noreply.github.com>
2017-08-02 16:53:33 -0700

    add pc usage logging scripts

---
+++ powershell/pc-usage-logging/logoff-logging.bat
@@ -0,0 +1,1 @@
+powershell -windowstyle hidden -executionpolicy bypass -file \\domain\netlogon\scripts\pc-usage-logging.ps1 -activity logoff
---
+++ powershell/pc-usage-logging/pc-usage-logging.ps1
@@ -0,0 +1,86 @@
+# Student Labs Record Logon/Logoff
+
+# Records basic user and computer data to a log file. 
+# This script is focussed on collecting data for tracking login/logouts but can 
+# be used generically with the activity argument being the name of any event you wish to track.
+# if the 2nd argument is 'test', will write to standard output and local home folder, 
+# if anything else (including blank), will write to log file
+
+# accepted parameters should be either "logon" or "logoff"
+# additional parameter available for testing with local log file
+param([string]$activity,[bool]$test=$false)
+
+# date formatting
+$activity_datetime = Get-Date -Format "MM/dd/yyyy hh:mm:ss tt"
+
+# network info
+$nic = Get-WmiObject -Class win32_networkadapterconfiguration -Filter "IPEnabled='True'" | where { $_.Description -like "Intel*"}
+$mac_address = $nic.MACAddress
+$ip_address = $nic.IPAddress[0]
+
+# computer name
+$computer_name = $env:computername
+
+# operating system info
+$os_name = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -Name ProductName).ProductName
+switch -Wildcard ($os_name) {
+    "Windows 7*" { $os_version = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -Name CurrentVersion).CurrentVersion }
+    "Windows 10*" { $os_version = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -Name ReleaseId).ReleaseId }
+    default { $os_version = "unknown" }
+}
+
+# hardware info
+$processor_info = (Get-WmiObject -Class win32_processor).name
+#$processor_type = (Get-WmiObject -Class win32_processor).caption
+
+# user info
+$username = $env:username
+$userinfo = ([ADSISEARCHER]"samaccountname=$($username)").Findone()
+# -replace throws away everything except the value following CN= (the 1st token)
+$usergroups = $userinfo.Properties.memberof -replace '^CN=([^,]+).+$','$1'
+
+# set lab prefix
+$lab = $computer_name.Substring(0,11)
+$labs = Import-Csv "\\server\share\folder\labs.csv" | where { $_.prefix -eq $lab } | select prefix,area,group
+# if no matches found, set to unknown
+if ($labs.area -eq "") {
+    $area = "UNKNOWN"
+} else {
+    $area = $labs.area
+}
+# set group membership status
+if ($usergroups -contains $labs.group) {
+    $member = "member"
+} else {
+    $member = "non-member"
+}
+
+# share info
+$drive_share = "U:\$username"
+
+# build line to write to log file
+$log_entry = "`"$area`",`"$username`",`"$member`",`"$activity`",`"$activity_datetime`",`"$computer_name`",`"$os_name`",`"$os_version`",`"$processor_info`",`"$drive_share`",`"$ip_address`",`"$mac_address`""
+
+# output data ----------------------------------------------
+if (!$test) {
+    $log_path = "\\server\share\folder"
+} else {
+    $log_path = "C:\logs"
+    if (!(Test-Path "C:\logs")) { New-Item -ItemType Directory -Path "C:\logs" | Out-Null }
+}
+
+# log file name
+# [area] - [YYYYMMDD].log
+$log_date = Get-Date -Format "yyyyMMdd"
+$log_filename = "$area - $log_date.log"
+
+# create log file with the header if it doesn't already exist
+if (!(Test-Path $log_path/$log_filename)) {
+    $log_header = "`"area`",`"username`",`"member`",`"activity`",`"activity_datetime`",`"computer_name`",`"os`",`"os_version`",`"processor_info`",`"drive_share`",`"ip_address`",`"mac_address`""
+    Write-Output $log_header | Out-File -Append $log_path\$log_filename
+    if ($test) { Write-Host $log_header }
+}
+
+# write to file
+Write-Output $log_entry | Out-File -Append $log_path\$log_filename
+if ($test) { Write-Host $log_entry }
