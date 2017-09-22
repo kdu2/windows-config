ec87433f6fed32fe4a0bab2d8bb0cd231ca169e1
kdu2 <kdu2@users.noreply.github.com>
2017-09-22 16:52:27 -0700

    add recompose script. update parameters for refresh script to include pool

--- powershell/VDI/Recompose-DesktopPool.ps1
+++ powershell/VDI/Recompose-DesktopPool.ps1
@@ -1,8 +1,8 @@
-param($ConnectionServer,$pool)
+param($ConnectionServer,$ParentVMPath,$ParentVMSnapshotPath,$delay)
 
 Add-PSSnapin vmware.view.broker
 Connect-VIServer $ConnectionServer
-Get-Pool -pool_id $pool | Get-DesktopVM | Send-LinkedCloneRefresh -schedule ((Get-Date).AddMinutes(1)) -ForceLogoff $true
+Get-Pool -pool_id $pool | Send-LinkedCloneRecompose -schedule ((Get-Date).AddMinutes($delay)) -parentVMPath $ParentVMPath  -parentSnapshotPath $ParentVMSnapshotPath -ForceLogoff $true -stopOnError $false
  
 $SmtpClient = New-Object system.net.mail.smtpClient
 $SmtpClient.host = "smtp.domain.com"   #Change to a SMTP server in your environment
@@ -10,6 +10,6 @@ $MailMessage = New-Object system.net.mail.mailmessage
 $MailMessage.from = "vdiDesktopSupport@domain.com"   #Change to email address you want emails to be coming from
 $MailMessage.To.add("vdiDesktopSupport@domain.com")    #Change to email address you want send to
 $MailMessage.IsBodyHtml = 1
-$MailMessage.Subject = "Refresh complete for $pool"    #Change to set your email subject
-$MailMessage.Body = "Refresh complete for $pool"    #Change to set the body message of the email
+$MailMessage.Subject = "Recompose complete for $pool"    #Change to set your email subject
+$MailMessage.Body = "Recompose complete for $pool"    #Change to set the body message of the email
 $SmtpClient.Send($MailMessage)
