eea4ac8b6b553e34a58c13d4a65ee702bbaeba97
kdu2 <kdu2@users.noreply.github.com>
2018-10-19 16:21:14 -0700

    update app list.

--- powershell/Config-W10.ps1
+++ powershell/Config-W10.ps1
@@ -70,76 +70,79 @@ $ErrorActionPreference = "SilentlyContinue"
 
 $apps = @(
     # default Windows 10 apps
-    #"Microsoft.3DBuilder"
+    "Microsoft.3DBuilder"
     "Microsoft.Appconnector"
     "Microsoft.BingFinance"
     "Microsoft.BingNews"
     "Microsoft.BingSports"
-    #"Microsoft.BingWeather"
-    "Microsoft.FreshPaint"
-    "Microsoft.BingTranslator"
+    "Microsoft.BingWeather"
+    #"Microsoft.FreshPaint"
     "Microsoft.Getstarted"
+    "Microsoft.BingTranslator"
     "Microsoft.MicrosoftOfficeHub"
     "Microsoft.MicrosoftSolitaireCollection"
+    "Microsoft.RemoteDesktop"
+    "Microsoft.MicrosoftPowerBIForWindows"
+    "Microsoft.MinecraftUWP"
     #"Microsoft.MicrosoftStickyNotes"
+    "Microsoft.NetworkSpeedTest"
     #"Microsoft.Office.OneNote"
     "Microsoft.OneConnect"
     "Microsoft.People"
-    "Microsoft.SkypeApp"
+    #"Microsoft.Print3D"
+    #"Microsoft.SkypeApp"
     #"Microsoft.Windows.Photos"
-    #"Microsoft.WindowsAlarms"
+    "Microsoft.WindowsAlarms"
     #"Microsoft.WindowsCalculator"
-    #"Microsoft.WindowsCamera"
+    "Microsoft.WindowsCamera"
+    "microsoft.windowscommunicationsapps"
     "Microsoft.WindowsMaps"
     "Microsoft.WindowsPhone"
     #"Microsoft.WindowsSoundRecorder"
     #"Microsoft.WindowsStore"
     "Microsoft.XboxApp"
+    "Microsoft.XboxGameOverlay"
+    "Microsoft.XboxIdentityProvider"
+    "Microsoft.XboxSpeechToTextOverlay"
     #"Microsoft.ZuneMusic"
     #"Microsoft.ZuneVideo"
-    "microsoft.windowscommunicationsapps"
-    "Microsoft.MinecraftUWP"
 
     # Threshold 2 apps
     "Microsoft.CommsPhone"
     "Microsoft.ConnectivityStore"
+    "Microsoft.GetHelp"
     "Microsoft.Messaging"
-    #"Microsoft.Office.Sway"
+    "Microsoft.Office.Sway"
+    "Microsoft.OneConnect"
     "Microsoft.WindowsFeedbackHub"
+    
+    # Creator's Update apps
+    #"Microsoft.MSPaint"
 
     # Redstone apps
     "Microsoft.BingFoodAndDrink"
     "Microsoft.BingTravel"
     "Microsoft.BingHealthAndFitness"
     "Microsoft.WindowsReadingList"
-    "Microsoft.NetworkSpeedTest"
-    "Microsoft.RemoteDesktop"
-    "Microsoft.MicrosoftPowerBIForWindows"
-
-    # Redstone 2 apps
+    "Microsoft.Whiteboard"
 
     # non-Microsoft
+    "9E2F88E3.Twitter"
     "PandoraMediaInc.29680B314EFC2"
-    "4DF9E0F8.Netflix"
-    "6Wunderkinder.Wunderlist"
-    "Duolingo"
-    "46928bounde.EclipseManager"
-    "D5EA27B7.Duolingo-LearnLanguagesforFree"
-    "Drawboard.DrawboardPDF"
-    "2FE3CB00.PicsArt-PhotoStudio"
-    "D52A8D61.FarmVille2CountryEscape"
-    "TuneIn.TuneInRadio"
-    "GAMELOFTSA.Asphalt8Airborne"
-    "*.Twitter"
     "Flipboard.Flipboard"
     "ShazamEntertainmentLtd.Shazam"
     "king.com.CandyCrushSaga"
     "king.com.CandyCrushSodaSaga"
     "king.com.*"
     "ClearChannelRadioDigital.iHeartRadio"
+    "4DF9E0F8.Netflix"
+    "6Wunderkinder.Wunderlist"
+    "Drawboard.DrawboardPDF"
+    "2FE3CB00.PicsArt-PhotoStudio"
+    "D52A8D61.FarmVille2CountryEscape"
+    "TuneIn.TuneInRadio"
+    "GAMELOFTSA.Asphalt8Airborne"
     "TheNewYorkTimes.NYTCrossword"
-    "AdobeSystemsIncorporated.AdobePhotoshopExpress"
-    "ActiproSoftwareLLC.562882FEEB491"
     "DB6EA5DB.CyberLinkMediaSuiteEssentials"
     "Facebook.Facebook"
     "flaregamesGmbH.RoyalRevolt2"
@@ -150,11 +153,14 @@ $apps = @(
     "XINGAG.XING"
     "89006A2E.AutodeskSketchBook"
     "D5EA27B7.Duolingo-LearnLanguagesforFree"
+    "46928bounde.EclipseManager"
+    "ActiproSoftwareLLC.562882FEEB491"
     "DolbyLaboratories.DolbyAccess"
     "SpotifyAB.SpotifyMusic"
     "A278AB0D.DisneyMagicKingdoms"
     "WinZipComputing.WinZipUniversal"
-
+    "AdobeSystemsIncorporated.AdobePhotoshopExpress"
+    
     # apps which cannot be removed using Remove-AppxPackage
     #"Microsoft.BioEnrollment"
     #"Microsoft.MicrosoftEdge"
@@ -180,7 +186,7 @@ foreach ($app in $apps) {
     Write-Host "Trying to remove $app" -ForegroundColor Green
     Get-AppxPackage -Name $app -AllUsers | Remove-AppxPackage
     Get-AppxPackage -Name $app | Remove-AppxPackage
-    Get-AppXProvisionedPackage -Online | where DisplayName -EQ $app | Remove-AppxProvisionedPackage -Online
+    Get-AppXProvisionedPackage -Online | where { $_.displayName -eq $app } | Remove-AppxProvisionedPackage -Online
     Write-Host "$app has been removed"
 }
 Write-Host ""
@@ -241,9 +247,14 @@ reg add hklm\software\policies\microsoft\windows\explorer /v NoNewAppAlert /t RE
 Write-Host ""
 
 # Disable OneDrive
-#Write-Host "Disabling OneDrive..." -ForegroundColor Green
-#reg add hklm\software\policies\microsoft\windows\OneDrive /v DisableFileSyncNGSC /t REG_DWORD /d 1 /f
-#Write-Host ""
+Write-Host "Disabling OneDrive..." -ForegroundColor Green
+reg add hklm\software\policies\microsoft\windows\OneDrive /v DisableFileSyncNGSC /t REG_DWORD /d 1 /f
+Write-Host ""
+
+# Disable Game DVR
+Write-Host "Disabling Game DVR..." -ForegroundColor Green
+reg add hklm\software\policies\microsoft\windows\GameDVR /v AllowgameDVR /t REG_DWORD /d 0 /f
+Write-Host ""
 
 # Disable automatic restart sign-on 
 Write-Host "Disabling automatic restart sign-on..." -ForegroundColor Cyan
@@ -259,23 +270,25 @@ reg add "hkcu\software\microsoft\windows\currentversion\notifications\settings\W
 # Disable OneDrive Setup
 Write-Host "Disabling OneDrive Setup..." -ForegroundColor Green
 reg delete "hku\temp\software\microsoft\windows\currentversion\run" /v "OneDriveSetup" /f
-reg delete "hkcu\software\microsoft\windows\currentversion\run" /v "OneDriveSetup" /f
-# Disable Game Mode
-Write-Host "Disabling Game Mode..."
+# Disable Game Bar and Game Mode
+Write-Host "Disabling Game Bar and Game Mode..." -ForegroundColor Green
+reg add "hku\temp\software\microsoft\windows\currentversion\GameDVR" /v AppCaptureEnabled /t REG_DWORD /d 0 /f
+reg add "hku\temp\system\GameConfigStore" /v GameDVR_Enabled /t REG_DWORD /d 0 /f
 reg add "hku\temp\software\microsoft\GameBar" /v AllowAutoGameMode /t REG_DWORD /d 0 /f
+reg add "hku\temp\software\microsoft\GameBar" /v AutoGameModeEnabled /t REG_DWORD /d 0 /f
 # Disable lock screen notifications
-Write-Host "Disabling lock screen notifications..."
+Write-Host "Disabling lock screen notifications..." -ForegroundColor Green
 reg add "hku\temp\software\microsoft\windows\currentversion\Notifications\Settings" /v "NOC_GLOBAL_SETTING_ALLOW_TOASTS_ABOVE_LOCK" /t REG_DWORD /d 0 /f
 reg add "hku\temp\software\microsoft\windows\currentversion\Notifications\Settings" /v "NOC_GLOBAL_SETTING_ALLOW_CRITICAL_TOASTS_ABOVE_LOCK" /t REG_DWORD /d 0 /f
 # Set feedback frequency to never
-Write-Host "Setting feedback frequency to never..."
+Write-Host "Setting feedback frequency to never..." -ForegroundColor Green
 reg add "hku\temp\software\microsoft\Siuf\Rules" /v "NumberOfSIUFInPeriod" /t REG_DWORD /d 0 /f
 reg add "hku\temp\software\microsoft\Siuf\Rules" /v "PeriodInNanoSeconds" /t REG_DWORD /d 0 /f
 # Set Explorer default to This PC instead of Quick Access
-Write-Host "Changing default Explorer view to This PC..."
+Write-Host "Changing default Explorer view to This PC..." -ForegroundColor Green
 reg add "hku\temp\software\microsoft\windows\currentversion\explorer\advanced" /v LaunchTo /t REG_DWORD /d 1 /f
 # Disable show frequent/recent files/folders in Quick Access
-Write-Host "Disabling show recent files/folders in Quick Access..."
+Write-Host "Disabling show recent files/folders in Quick Access..." -ForegroundColor Green
 reg add "hku\temp\software\microsoft\windows\currentversion\explorer" /v ShowFrequent /t REG_DWORD /d 0 /f
 reg add "hku\temp\software\microsoft\windows\currentversion\explorer" /v ShowRecent /t REG_DWORD /d 0 /f
 # Show file extension in explorer
@@ -283,8 +296,8 @@ Write-Host "Enabling show file extension in explorer..." -ForegroundColor Green
 reg add "hku\temp\software\microsoft\windows\currentversion\explorer\advanced" /v HideFileExt /d 0 /t REG_DWORD /f
 # Show user files shortcut on desktop
 Write-Host "Enable show user file icons on desktop..." -ForegroundColor Green
-reg add "hku\temp\software\microsoft\windows\currentversion\explorer\HideDesktopIcons\NewStartPanel" /v "{59031a47-3f72-44a7-89c5-5595fe6b30ee}" /d /t REG_DWORD /f
-reg add "hku\temp\software\microsoft\windows\currentversion\explorer\HideDesktopIcons\ClassicStartMenu" /v "{59031a47-3f72-44a7-89c5-5595fe6b30ee}" /d /t REG_DWORD /f
+reg add "hku\temp\software\microsoft\windows\currentversion\explorer\HideDesktopIcons\NewStartPanel" /v "{59031a47-3f72-44a7-89c5-5595fe6b30ee}" /d 0  /t REG_DWORD /f
+reg add "hku\temp\software\microsoft\windows\currentversion\explorer\HideDesktopIcons\ClassicStartMenu" /v "{59031a47-3f72-44a7-89c5-5595fe6b30ee}" /d 0 /t REG_DWORD /f
 # Show This PC shortcut on desktop
 Write-Host "Enable show This PC icon on desktop" -ForegroundColor Green
 reg add "hku\temp\software\microsoft\windows\currentversion\explorer\HideDesktopIcons\NewStartPanel" /v "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" /d 0 /t REG_DWORD /f
@@ -296,11 +309,16 @@ reg add "hku\temp\software\microsoft\windows\currentversion\explorer\advanced" /
 reg add "hku\temp\software\microsoft\windows\currentversion\explorer\advanced" /v ShowTaskViewButton /d 0 /t REG_DWORD /f
 reg add "hku\temp\software\microsoft\windows\currentversion\explorer\advanced\People" /v PeopleBand /d 0 /t REG_DWORD /f
 reg add "hku\temp\software\Microsoft\Windows\CurrentVersion\Search" /v SearchboxTaskbarMode /d 0 /t REG_DWORD /f 
+# Set IE 11 home page
+Write-Host "Setting Internet Explorer 11 home page" -ForegroundColor Green
+reg add "hku\temp\software\microsoft\Internet Explorer" /v "Start Page" /d "http://www.cccd.edu" /t REG_SZ /f
 # Disable Edge first run
 Write-Host "Disabling Microsoft Edge first run..." -ForegroundColor Green
 reg add "hku\temp\software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\FirstRun" /v "LastFirstRunVersionDelivered" /t REG_DWORD /d 1 /f
 reg add "hku\temp\software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\Main" /v "IE10TourShown" /t REG_DWORD /d 1 /f
 reg add "hku\temp\software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\Main" /v "DisallowDefaultBrowserPrompt" /t REG_DWORD /d 1 /f
+#reg add "hku\temp\software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\Main" /v "HomeButtonEnabled" /t REG_DWORD /d 1 /f
+#reg add "hku\temp\software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\Main" /v "HomeButtonPage" /t REG_SZ /d "https://www.cccd.edu" /f
 # Disable 3rd party apps
 Write-Host "Disabling 3rd party apps..." -ForegroundColor Green
 reg add "hku\temp\software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SystemPaneSuggestionsEnabled /t REG_DWORD /d 0 /f
@@ -344,18 +362,18 @@ reg add hklm\system\currentcontrolset\control\network\NewNetworkWindowOff
 Write-Host ""
 
 # Set power configuration
-Write-Host "Disabling Hibernate"
-powercfg -h off
+#Write-Host "Disabling Hibernate"
+#powercfg -h off
 Write-Host "Setting monitor timeout"
 powercfg -change -monitor-timeout-ac 30
-Write-Host "Disabling sleep timeout"
-powercfg -change -standby-timeout-ac 0
+#Write-Host "Disabling sleep timeout"
+#powercfg -change -standby-timeout-ac 0
 Write-Host ""
 
 # Enable RDP
-Write-Host "Enabling RDP" -ForegroundColor Green
-reg add "hklm\system\currentcontrolset\control\terminal server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
-Write-Host ""
+#Write-Host "Enabling RDP" -ForegroundColor Green
+#reg add "hklm\system\currentcontrolset\control\terminal server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+#Write-Host ""
 
 # Did this break?:
 If ($NoWarn -eq $False)
