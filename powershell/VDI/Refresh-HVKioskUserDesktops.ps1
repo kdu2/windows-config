5ac43089d8b7d1451e54b9e475348b7cd8d3a43a
kdu2 <kdu2@users.noreply.github.com>
2019-08-06 09:30:12 -0700

    switch to send-mailmessage.

--- powershell/VDI/Refresh-HVKioskUserDesktops.ps1
+++ powershell/VDI/Refresh-HVKioskUserDesktops.ps1
@@ -19,12 +19,11 @@ foreach ($session in $kiosksessions) {
 
 Start-HVPool -Refresh -Pool $pool -Machines $desktops -LogoffSetting FORCE_LOGOFF -Confirm:$false
 
-$SmtpClient = New-Object system.net.mail.smtpClient
-$SmtpClient.host = "SMTP_HOST"                    # Change to a SMTP server in your environment
-$MailMessage = New-Object system.net.mail.mailmessage
-$MailMessage.from = "SENDER_EMAIL"      # Change to email address you want emails to be coming from
-$MailMessage.To.add("RECIPIENT_EMAIL")     # Change to email address you want send to
-$MailMessage.IsBodyHtml = 1
-$MailMessage.Subject = "Refresh complete for $username in $pool"   # Change to set your email subject
-$MailMessage.Body = "Refresh complete for $username in $pool"      # Change to set the body message of the email
-$SmtpClient.Send($MailMessage)
+$sendmailparams = @{
+    SMTPServer = "SMTP_HOST"
+    From = "SENDER_EMAIL"
+    To = "RECIPIENT_EMAIL"
+    Subject = "Refresh complete for $username in $pool"
+    Body = "Refresh complete for $username in $pool"
+}
+Send-MailMessage @sendmailparams
