c84ef9507c42e19eae5a88c3cce490c6fb6a35e0
kdu2 <kdu2@users.noreply.github.com>
2021-01-04 11:07:31 -0800

    fix delete, switch to txt

--- cmd/vsnotes_calendar.bat
+++ cmd/vsnotes_calendar.bat
@@ -7,20 +7,20 @@ for %%m in (01-January 02-February 03-March 04-April 05-May 06-June 07-July 08-A
 	mkdir %year%\%%m	
 	set mtemp=%%m
 	set mfolder=!mtemp:~3!
-	for /l %%d in (1,1,9) do (type nul>> %year%\%%m\!mfolder!_0%%d_%year%.md)
-	for /l %%d in (10,1,31) do (type nul>> %year%\%%m\!mfolder!_%%d_%year%.md)
+	for /l %%d in (1,1,9) do (type nul>> %year%\%%m\!mfolder!_0%%d_%year%.txt)
+	for /l %%d in (10,1,31) do (type nul>> %year%\%%m\!mfolder!_%%d_%year%.txt)
     endlocal
 )
 
-if %leapyear% == 'n' del 02-February\February_29_%year%.md /q /s
+if %leapyear% == 'n' del %year%\02-February\February_29_%year%.txt /q /s
 
 for %%f in (
-    02-February\February_30_%year%.md
-    02-February\February_31_%year%.md
-    04-April\April_31_%year%.md
-    06-June\June_31_%year%.md
-    09-September\September_31_%year%.md
-    11-November\November_31_%year%.md    
-) do (del %%f /q /s)
+    02-February\February_30_%year%.txt
+    02-February\February_31_%year%.txt
+    04-April\April_31_%year%.txt
+    06-June\June_31_%year%.txt
+    09-September\September_31_%year%.txt
+    11-November\November_31_%year%.txt    
+) do (del %year%\%%f /q /s)
 
 pause
