8214f4f4ad4ba5205923d6b8888dcac921fc4214
kdu2 <kdu2@users.noreply.github.com>
2021-04-16 14:06:07 -0700

    add sync

---
+++ powershell/Files/sync-vsnotes.ps1
@@ -0,0 +1,8 @@
+Set-Location $env:userprofile\github\vsnotes
+$gstat = git status --porcelain
+if ($gstat.length -ne 0) {
+    git add --all
+    git commit -m "$gstat"
+    git pull
+    git push origin main
+}
